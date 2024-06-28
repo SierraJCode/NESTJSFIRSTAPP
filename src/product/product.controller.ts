a7dca03b613c8a387809c20da8bd79ee00a8d70d
Jesus Sierra <sierraja06@gmail.com>
2024-06-28 15:18:06 -0500

    Metodo crear producto funcionando pero sin validacion

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,14 +1,20 @@
 import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
 import { CreateProductDTO } from './dto/product.dto';
+import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
 
+    constructor(private productService: ProductService){}
+
     @Post()
-    createPost(@Res() res, @Body() createProductDTO: CreateProductDTO){
-        console.log(createProductDTO);
+    async createPost(@Res() res, @Body() createProductDTO: CreateProductDTO){
+        
+        const product = await this.productService.createProduct(createProductDTO)
+
         return res.status(HttpStatus.OK).json({
-            message: 'Vrg.. todo salió bien.'
+            message: 'Vrg.. todo salió bien.',
+            product: product
         });
     }
 
